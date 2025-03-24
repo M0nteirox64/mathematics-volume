90757fd334dce497f87469c10bba1d7246346132
MonteiroPSV <trxshroot@gmail.com>
2025-03-24 22:47:07 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <Windows.h>
+# define M_PI           3.14159265358979323846
+
+
+int main() {
+
+    int chc;
+
+    std::cout << "1. calcular area\n";
+    std::cout << "> ";
+    std::cin >> chc;
+
+    if (chc) {
+        std::cout << "(piramide quadrangular)\n";
+        int L;
+        int H;
+    
+        std::cout << "lado: ";
+        std::cin >> L;
+        std::cout << "altura: ";
+        std::cin >> H;
+ 
+        int Ab = L * L;
+        int V = Ab * H / 3;
+
+        system("cls");
+        std::cout << "Area da base: " << Ab << "\n";
+        std::cout << "Volume: " << V << "\n"; 
+        Sleep(3333);
+    }
+}
+
+
+
